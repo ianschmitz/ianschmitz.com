3e36df16ea6d78e8da27b1e3ce92a265fd03c101
Ian Schmitz <ianschmitz@gmail.com>
2018-10-08 15:01:25 -0700

    Cleanup web app manifest and add gtag support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,28 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Home | Ian Schmitz',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
-      },
+    siteMetadata: {
+        title: "Home | Ian Schmitz",
     },
-    'gatsby-plugin-offline',
-  ],
-}
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        {
+            resolve: `gatsby-plugin-gtag`,
+            options: {
+                trackingId: "UA-127027199-1",
+                anonymize: true,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "ianschmitz.com",
+                short_name: "ianschmitz.com",
+                start_url: "/",
+                background_color: "#31313a",
+                theme_color: "#31313a",
+                display: "minimal-ui",
+                icon: "src/images/favicon.png", // This path is relative to the root of the site.
+            },
+        },
+        "gatsby-plugin-offline",
+    ],
+};
